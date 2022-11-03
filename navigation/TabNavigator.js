5e5db65073c4b90e2633b39a8e68a811820a05ba
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-11-03 11:01:45 -0300

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Feed from "../screens/Feed";
+import CreatePost from "../screens/CreatePost";
+import { RFValue } from "react-native-responsive-fontsize";
+
+const Tab = createMaterialBottomTabNavigator();
+
+import firebase from "firebase";
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Feed') {
+                        iconName = focused
+                            ? 'book'
+                            : 'book-outline';
+                    } else if (route.name === 'Criar Postagem') {
+                        iconName = focused ? 'create' : 'create-outline';
+                    }
+                    return <Ionicons name={iconName} size={RFValue(20)} color={color} />;
+                },
+            })}
+            backgroundColor={""}//eu
+            activeColor={"tomato"}
+            inactiveColor={"#ACD"}//"gray"
+            tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: '#ACD',//'gray'
+            }}
+        >
+            <Tab.Screen name="Feed" component={Feed} />
+            <Tab.Screen name="Criar Postagem" component={CreatePost} />
+        </Tab.Navigator >
+    );
+}
+
+export default BottomTabNavigator;
